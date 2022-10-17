c226b30f106871ffbb15c0c7e46e4e1db96ad3ee
BacNg <bac@languageconfidence.com>
2022-10-17 10:10:22 +0000

    clean up func

--- infra/lambda/read_audio.js
+++ infra/lambda/read_audio.js
@@ -1,5 +1,3 @@
-console.log('Loading function');
-        
 const aws = require('aws-sdk');
 
 const s3 = new aws.S3({ apiVersion: '2006-03-01' });
@@ -7,7 +5,6 @@ const s3 = new aws.S3({ apiVersion: '2006-03-01' });
 exports.handler = async (event, context) => {
     console.log('Received event:', JSON.stringify(event, null, 2));
 
-    // Get the object from the event and show its content type
     const bucket = event.Records[0].s3.bucket.name;
     const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
     const params = {
@@ -21,8 +18,6 @@ exports.handler = async (event, context) => {
         return ContentType;
     } catch (err) {
         console.log(err);
-        const message = `Error getting object ${key} from bucket ${bucket}. Make sure they exist and your bucket is in the same region as this function.`;
-        console.log(message);
         throw new Error(message);
     }
 };
