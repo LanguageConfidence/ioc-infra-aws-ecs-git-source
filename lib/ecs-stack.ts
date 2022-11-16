b501c0fea3d80d2d6b6e6e38626b5652a4827530
Nguyễn Việt Bắc <86348885+egochao@users.noreply.github.com>
2022-11-16 08:16:21 +0000

    remove vpc

--- lib/ecs-stack.ts
+++ lib/ecs-stack.ts
@@ -7,25 +7,15 @@ import * as ecs from "aws-cdk-lib/aws-ecs";
 export class EcsStack extends cdk.Stack {
 
   public readonly myCluster: ecs.Cluster;
-  public readonly myVpc: ec2.Vpc;
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    this.myVpc = new ec2.Vpc(this, "EcsVpc", {
-        maxAzs: 3 // Default is all AZs in region
-    });
   
-    this.myCluster = new ecs.Cluster(this, "EcsCluster", {
-        vpc: this.myVpc,
-    });
+    this.myCluster = new ecs.Cluster(this, "EcsCluster", {});
 
     new cdk.CfnOutput(this, 'ClusterName', {
         value: this.myCluster.clusterName,
-    });
-    new cdk.CfnOutput(this, 'VpcId', {
-        value: this.myVpc.vpcId,
-    });
-    
+    });    
   }
 }
