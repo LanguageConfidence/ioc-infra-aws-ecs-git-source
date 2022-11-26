449f2d3787a423dd7cd3e6550584560febee2a70
Nguyễn Việt Bắc <86348885+egochao@users.noreply.github.com>
2022-11-26 06:33:11 +0000

    add hello lambda func

--- lib/apigwlambda.ts
+++ lib/apigwlambda.ts
@@ -1,20 +1,31 @@
-import * as apigatewayv2 from 'aws-cdk-lib/aws-apigatewayv2';
-
+import * as apigatewayv2 from '@aws-cdk/aws-apigatewayv2-alpha';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as cdk from 'aws-cdk-lib';
 import * as path from 'path';
-import { HttpUrlIntegration, HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
+import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
 
 export class ApigwLambdaMiddleware extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const hello = new lambda.Function(this, 'HelloHandler', {
+    const hello_fn = new lambda.Function(this, 'HelloHandler', {
       runtime: lambda.Runtime.NODEJS_16_X,
       code: lambda.Code.fromAsset(path.join(__dirname, 'lambda')),
       handler: 'hello.handler',
     });
 
+    const gwLambdaIg = new HttpLambdaIntegration('BooksIntegration', hello_fn);
+    const httpApi = new apigatewayv2.HttpApi(this, 'HttpApi');
 
+    httpApi.addRoutes({
+      path: '/',
+      methods: [apigatewayv2.HttpMethod.ANY],
+      integration: gwLambdaIg,
+    }); 
+
+
+    new cdk.CfnOutput(this, 'HttpApiUrl', {
+      value: httpApi.apiEndpoint ?? 'Something went wrong with the deploy',
+    });
   }
 }
--- lib/lambda/hello.ts
+++
@@ -1,5 +0,0 @@
-exports.handler = async (event, context) => {
-    console.log('Received event:', JSON.stringify(event, null, 2));
-    return "Hi this is Bac apigw endpoint";
-};
-              
